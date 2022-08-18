a76e361869d6d09ebb3338096f58e0edd30980a9
Mele <15926661116@163.com>
2022-08-19 02:53:54 +0800

    0.0.2-SNAPSHOT

--- id-generate-spring-boot-starter/src/main/java/io/github/aapplet/segment/IdGenerateAutoConfiguration.java
+++ id-generate-spring-boot-starter/src/main/java/io/github/aapplet/segment/IdGenerateAutoConfiguration.java
@@ -4,18 +4,31 @@ import io.github.aapplet.segment.core.IdGenerator;
 import io.github.aapplet.segment.core.IdProduction;
 import io.github.aapplet.segment.loader.SegmentLoader;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 public class IdGenerateAutoConfiguration {
 
     @Bean
-    IdGenerator idGenerator(SegmentLoader segmentLoader) {
+    IdGenerator idGenerator(SegmentLoader segmentLoader, ObjectProvider<IdGenerateCustomizer> idGenerateCustomizers) {
+        idGenerateCustomizers.forEach(idGenerateCustomizer -> {
+            idGenerateCustomizer.customize(null);
+        });
         return new IdProduction(segmentLoader);
     }
 
     @Bean
     SegmentManager segmentManager(SqlSessionTemplate sqlSessionTemplate) {
         return new SegmentManager(sqlSessionTemplate);
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    IdGenerateCustomizer idGenerateCustomizer() {
+        return obj -> {
+        };
     }
 
 }
---
+++ id-generate-spring-boot-starter/src/main/java/io/github/aapplet/segment/IdGenerateCustomizer.java
@@ -0,0 +1,8 @@
+package io.github.aapplet.segment;
+
+@FunctionalInterface
+public interface IdGenerateCustomizer {
+
+    void customize(Object obj);
+
+}
